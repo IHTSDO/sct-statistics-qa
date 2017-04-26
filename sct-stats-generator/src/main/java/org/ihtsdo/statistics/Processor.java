f09d7324bd45c748eb51202150c0804cb9790c6e
ale-git-user <arodriguez@termmed.com>
2017-04-26 15:09:38 -0300

    Added control for division by zero and filter to total active rels

--- sct-stats-generator/src/main/java/org/ihtsdo/statistics/Processor.java
+++ sct-stats-generator/src/main/java/org/ihtsdo/statistics/Processor.java
@@ -195,7 +195,12 @@ public class Processor {
 				ResultSetMetaData meta= rs.getMetaData();
 				while(rs.next()){
 					for (int i=0;i<meta.getColumnCount();i++){
-						bw.append(rs.getObject(i+1).toString().replaceAll(",","&#44;").trim());
+
+						if (rs.getObject(i+1)!=null){
+							bw.append(rs.getObject(i+1).toString().replaceAll(",","&#44;").trim());
+						}else{
+							bw.append("");
+						}
 						if (i+1<meta.getColumnCount()){
 							bw.append(",");
 						}else{
@@ -256,7 +261,12 @@ public class Processor {
 					ResultSetMetaData meta= rs.getMetaData();
 					while(rs.next()){
 						for (int i=0;i<meta.getColumnCount();i++){
-							bw.append(rs.getObject(i+1).toString());
+
+							if (rs.getObject(i+1)!=null){
+								bw.append(rs.getObject(i+1).toString());
+							}else{
+								bw.append("");
+							}
 							if (i+1<meta.getColumnCount()){
 								bw.append(",");
 							}else{
